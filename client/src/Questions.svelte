c6cb16052032cbfd087ded610a9ac18feab90bf9
Alexander Horn <alexander.horn@conplement.de>
2023-02-26 19:14:47 +0100

    add event listener for new question

--- client/src/Questions.svelte
+++ client/src/Questions.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
     import { each } from "svelte/internal";
+    import { get } from "svelte/store";
+    import { accessToken, idToken } from "./lib/auth/auth";
     import { getQuestions, questions } from "./lib/questions";
 
-    $: promise = getQuestions();
+    const eventSource = new EventSource("http://localhost:3333/api/v1/events", {
+        headers: {
+            Authorization: `Bearer ${get(idToken)}`,
+        },
+    } as any);
+
+    $: promise = getQuestions(eventSource);
 </script>
 
 {#each $questions as question}
-    <div>{question.Text}</div>
+    <div>{JSON.stringify(question)}</div>
 {/each}
--- client/src/lib/questions.ts
+++ client/src/lib/questions.ts
@@ -1,20 +1,28 @@
-import { writable } from "svelte/store"
+import { get, writable, type Writable } from "svelte/store"
+import type { Question } from "../models/question"
 import { idToken } from "./auth/auth"
 
-export const questions = writable([])
+export const questions: Writable<Question[]> = writable([])
 export const sessionActive = writable(false)
+let source: EventSource | null = null
 
-export const getQuestions = async () => {
-	let token = ""
-	const unsub = idToken.subscribe((value) => {
-		token = value
+export const getQuestions = async (eventSource: EventSource) => {
+	source = eventSource
+	source.addEventListener("new_question", (event) => {
+		const data = JSON.parse(event.data)
+		questionAdded(data)
 	})
 	const repsonse = await fetch("http://localhost:3333/api/v1/question/session", {
 		headers: {
-			Authorization: `Bearer ${token}`
+			Authorization: `Bearer ${get(idToken)}`
 		}
 	})
 	const data = await repsonse.json()
-	questions.set(data)
-	unsub()
+	questions.set(data as Question[])
+}
+
+const questionAdded = (question: Question) => {
+	questions.update((questions) => {
+		return (questions = [...questions, question])
+	})
 }
