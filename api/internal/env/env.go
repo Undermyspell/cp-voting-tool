4fa126966358e293ca0ba23c11e627e114020e91
Michael Kaiser <michael.kaiser@conplement.de>
2023-04-03 21:58:35 +0200

    capitalize secret names

--- api/internal/env/env.go
+++ api/internal/env/env.go
@@ -8,8 +8,8 @@ const (
 	USE_MOCK_JWKS            string = "USE_MOCK_JWKS"
 	JWKS_URL                 string = "JWKS_URL"
 	VOTING_STORAGE_IN_MEMORY string = "VOTING_STORAGE_IN_MEMORY"
-	REDIS_ENPOINT            string = "REDIS_ENDPOINT_secret"
-	REDIS_PASSWORD           string = "REDIS_PASSWORD_secret"
+	REDIS_ENPOINT            string = "REDIS_ENDPOINT_SECRET"
+	REDIS_PASSWORD           string = "REDIS_PASSWORD_SECRET"
 )
 
 type EnvConfig struct {
