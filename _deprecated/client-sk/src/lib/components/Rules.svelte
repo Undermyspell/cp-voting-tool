b0ce9fc01f930d2c87a024e330b76c4bf17e3694
Michael Kaiser <michael.kaiser@conplement.de>
2025-02-18 15:20:47 +0100

    update dependencies (#280)

--- _deprecated/client-sk/src/lib/components/Rules.svelte
+++
@@ -1,9 +0,0 @@
-<div>
-	<ul class="list-disc">
-		<li>Achte bei deinen Fragen immer auf einen fairen und höflichen Ton und bleibe sachlich.</li>
-		<li>Unsachliche und/oder beleidigende Fragen haben bei uns keinen Platz.</li>
-		<li>Wahre die Regeln der Höflichkeit, wie in einem Gespräch von Angesicht zu Angesicht.</li>
-		<li>Sieh davon ab, andere persönlich zu provozieren.</li>
-		<li>Gib deinen Namen an, um für Rückfragen ansprechbar zu sein.</li>
-	</ul>
-</div>
--- _deprecated/client-sk/src/lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export class Constants {
-	static readonly QuestionMaxLength = 500;
-}
